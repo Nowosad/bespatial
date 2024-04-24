b57773c3e99e73d1ed90a85099ee93421b8ba54e
Jakub Nowosad <tupiszakaczki@gmail.com>
2024-04-24 14:32:24 +0200

    cleans package

--- R/to_terra.R
+++ R/to_terra.R
@@ -1,24 +1,30 @@
 to_terra = function(x) UseMethod("to_terra")
+#' @export
 to_terra.RasterLayer = function(x) {
   x = terra::rast(x)
   return(x)
 }
+#' @export
 to_terra.RasterStack = function(x) {
   x = terra::rast(x)
   return(x)
 }
+#' @export
 to_terra.RasterBrick = function(x) {
   x = terra::rast(x)
   return(x)
 }
+#' @export
 to_terra.stars = function(x) {
   x = terra::rast(x)
   return(x)
 }
+#' @export
 to_terra.matrix = function(x) {
   x = terra::rast(x)
   return(x)
 }
+#' @export
 to_terra.array = function(x) {
   x = terra::rast(x)
   return(x)
